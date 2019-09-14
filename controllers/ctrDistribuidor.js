f57f2a0dfdfb893eeb3263f624ae1dbecb943f22
Yair <yair_918@hotmail.com>
2019-09-13 22:27:13 -0500

    Refactoring code and schema response

--- controllers/ctrDistribuidor.js
+++ controllers/ctrDistribuidor.js
@@ -2,54 +2,52 @@ const Db = require('../utility/db');
 const { ObjectID } = require('mongodb');
 const { errorHandler } = require('../utility/errorHandler');
 const collectionName = 'distribuidor';
+const className = 'CtrDistribuidor';
 
 class CtrDistribuidor {
 
-    static add(root, args) {
-        return new Promise(async resolve => {
-            let db = new Db();
-            try {
-                let objCnn = await db.openConnection();
-                if (!objCnn.status) {
-                    errorHandler({
-                        method: 'CtrDistribuidor.add',
-                        message: `Error connecting to database ${objCnn.message}`
-                    });
-                    resolve(`Error has ocurred!`);
-                    return;
-                }
-                let collection = objCnn.db.collection(collectionName);
-                let newObj = {
-                    nombre: args.input.nombre,
-                    id_fiscal: args.input.id_fiscal,
-                    create_at: new Date(),
-                    updated_at: new Date()
-                }
-                collection.insertOne(newObj, (err, result) => {
-                    if (err) {
-                        errorHandler({
-                            method: 'CtrDistribuidor.add',
-                            message: `Error executing query ${err}`
-                        });
-                        resolve(`Error has ocurred!`);
-                        return;
-                    }
-                    resolve({
-                        _id: ObjectID(result.insertedId),
-                        nombre: args.input.nombre,
-                        id_fiscal: args.input.id_fiscal
-                    });
-                });
-            } catch (error) {
+    static async add(root, args) {
+        let exist = await CtrDistribuidor.validateIfExist({ id_fiscal: args.input.id_fiscal });
+        if (exist) {
+            return {
+                status: false,
+                message: `El nit ya existe enla base de datos!`,
+                _id: null
+            };
+        }
+        let db = new Db();
+        try {
+            let objCnn = await db.openConnection();
+            if (!objCnn.status) {
                 errorHandler({
-                    method: 'CtrDistribuidor.add',
-                    message: `Unexpected error -> ${error}`
+                    method: `${className}.add`,
+                    message: `Error connecting to database ${objCnn.message}`
                 });
-                resolve(`Error has ocurred!`);
-            } finally {
-                db.closeConnection();
             }
-        });
+            let collection = objCnn.db.collection(collectionName);
+            let newObj = {
+                nombre: args.input.nombre,
+                id_fiscal: args.input.id_fiscal,
+                create_at: new Date(),
+                updated_at: new Date()
+            }
+            let result = await collection.insertOne(newObj);
+            if (result) {
+                return {
+                    status: true,
+                    message: null,
+                    _id: ObjectID(result.insertedId)
+                }
+
+            }
+        } catch (error) {
+            errorHandler({
+                method: `${className}.add`,
+                message: `Unexpected error -> ${error}`
+            });
+        } finally {
+            db.closeConnection();
+        }
     }
 
     static async update(root, input) {
@@ -58,67 +56,89 @@ class CtrDistribuidor {
             let objCnn = await db.openConnection();
             if (!objCnn.status) {
                 errorHandler({
-                    method: 'CtrDistribuidor.update',
+                    method: `${className}.update`,
                     message: `Error connecting to database ${objCnn.message}`
                 });
-                return `Error has ocurred!`;
             }
             let collection = objCnn.db.collection(collectionName);
             let result = await collection.updateOne(
                 { _id: ObjectID(input._id) },
                 { $set: { ...input.input, updated_at: new Date() } }
             );
-            if (result.matchedCount > 0) {
-                result = await collection.findOne({ _id: ObjectID(input._id) });
+            if (result && result.matchedCount > 0) {
+                return {
+                    status: true,
+                    message: `Distribuidor actualizado de forma exitosa!`
+                };
             }
-            return result;
+            return {
+                status: false,
+                message: `No ha sido posible actualizar el distribuidor.`
+            };
         } catch (error) {
             errorHandler({
-                method: 'CtrDistribuidor.update',
+                method: `${className}.update`,
                 message: `Unexpected error -> ${error}`
             });
-            return `Error has ocurred!`;
         } finally {
             db.closeConnection();
         }
     }
 
-    static delete(root, input) {
-        return new Promise(async resolve => {
-            let db = new Db();
-            try {
-                let objCnn = await db.openConnection();
-                if (!objCnn.status) {
-                    errorHandler({
-                        method: 'CtrDistribuidor.delete',
-                        message: `Error connecting to database ${objCnn.message}`
-                    });
-                    resolve({ message: `Error has ocurred!` });
-                }
-                let collection = objCnn.db.collection(collectionName);
-                collection.deleteOne({ _id: ObjectID(input._id) },
-                    (err, obj) => {
-                        if (err) {
-                            errorHandler({
-                                method: 'CtrDistribuidor.delete',
-                                message: `Error ->  ${err}`
-                            });
-                        }
-                        resolve({
-                            wasDelete: true
-                        });
-                    }
-                );
-            } catch (error) {
+    static async delete(root, input) {
+        let db = new Db();
+        try {
+            let objCnn = await db.openConnection();
+            if (!objCnn.status) {
                 errorHandler({
-                    method: 'CtrDistribuidor.update',
-                    message: `Unexpected error -> ${error}`
+                    method: `${className}.delete`,
+                    message: `Error connecting to database ${objCnn.message}`
                 });
-                return false;
-            } finally {
-                db.closeConnection();
             }
-        });
+            let collection = objCnn.db.collection(collectionName);
+            let result = await collection.deleteOne({ _id: ObjectID(input._id) });
+            if (!result) {
+                return {
+                    status: false,
+                    message: `No ha sido posible eliminar el distribuidor!`,
+                };
+            }
+            return {
+                status: true,
+                message: `El distribuidor ha sido borrado!`
+            };
+        } catch (error) {
+            errorHandler({
+                method: `${className}.delete`,
+                message: `Unexpected error -> ${error}`
+            });
+        } finally {
+            db.closeConnection();
+        }
+    }
+
+    static async validateIfExist(params) {
+        let db = new Db();
+        let objCnn = await db.openConnection();
+        if (!objCnn.status) {
+            errorHandler({
+                method: `${className}.validateIfExist`,
+                message: `Error connecting to database ${objCnn.message}`
+            });
+        }
+        let collection = objCnn.db.collection(collectionName);
+        try {
+            let result = await collection.findOne(params);
+            if (result)
+                return true
+            else
+                return false
+        } catch (error) {
+            errorHandler({
+                method: `${className}.validateIfExist`,
+                message: `Error connecting to database ${objCnn.message}`
+            });
+        }
     }
 }
 
