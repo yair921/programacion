72178906d78267b04543fe3822bd448bed5c85ec
Yair <yair_918@hotmail.com>
2019-09-17 21:25:45 -0500

    Add CtrPeliculaFormato

--- gql/mutations.js
+++ gql/mutations.js
@@ -1,6 +1,7 @@
 const ctrDistribuidor = require('../controllers/ctrDistribuidor');
 const ctrSalaTipo = require('../controllers/ctrSalaTipo');
 const ctrTeatro = require('../controllers/ctrTeatro');
+const ctrPeliculaFormato = require('../controllers/ctrPeliculaFormato');
 
 module.exports = {
     addDistribuidor: ctrDistribuidor.add,
@@ -11,5 +12,8 @@ module.exports = {
     deleteSalaTipo: ctrSalaTipo.delete,
     addTeatro: ctrTeatro.add,
     updateTeatro: ctrTeatro.update,
-    deleteTeatro: ctrTeatro.delete
+    deleteTeatro: ctrTeatro.delete,
+    addPeliculaFormato: ctrPeliculaFormato.add,
+    updatePeliculaFormato: ctrPeliculaFormato.update,
+    deletePeliculaFormato: ctrPeliculaFormato.delete
 }
--- gql/queries.js
+++ gql/queries.js
@@ -1,10 +1,12 @@
 const ctrDistribuidor = require('../controllers/ctrDistribuidor');
 const ctrTeatro = require('../controllers/ctrTeatro');
 const ctrSalaTipo = require('../controllers/ctrSalaTipo');
+const ctrPeliculaFormato = require('../controllers/ctrPeliculaFormato');
 
 
 module.exports = {
     getAllDistribuidor: ctrDistribuidor.getAll,
     getAllSalaTipo: ctrSalaTipo.getAll,
-    getAllTeatro: ctrTeatro.getAll
+    getAllTeatro: ctrTeatro.getAll,
+    getAllPeliculaFormato: ctrPeliculaFormato.getAll
 }
